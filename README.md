7fa026c94f29dd9c3694a538993de0036df6ae85
dawc <dawc@wwu.edu>
2017-07-31 16:26:49 -0700

    Add the script and update the readme.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # TimingScript
-zsh script to time any command any number of times
+Zsh script to time any command any number of times.
+
+* After the script is finished, it will print out the total time elapsed and the average time.
+* By default, the script runs the command 5 times.
+* If an error is thrown by the command, the script exits.
+
+## Usage
+
+Skeleton:
+```sh
+ ./timingscript.sh [options] <command>
+ ```
+
+Example:
+
+```sh
+./timingscript.sh -r 30 python do-calculus.py integrals.json
+```
---
+++ timingscript.sh
@@ -0,0 +1,51 @@
+#!/bin/zsh
+# Variables
+TOTALTIME=0
+uTOTALTIME=0
+REPS=5
+FORCE=false
+
+# Handle user input for repitition
+while getopts :r:f repititions; do
+    case $repititions in
+        r)
+            REPS=$OPTARG
+            shift 2
+            ;;
+        f)
+            FORCE=true
+            shift 1
+            ;;
+        \?)
+            continue
+            ;;
+    esac
+done
+
+COMMAND=$@
+
+# Test that the command works
+eval $COMMAND > /dev/null
+EXIT=$?
+if [ "$FORCE" != true ] ; then
+    if [ "$EXIT" -eq "1" ] ; then
+        printf "The command \`$COMMAND\` throws an error.\n"
+        printf "Exiting with code 1.\n"
+        exit 1
+    fi
+fi
+
+# Alert user that the command is running
+printf "Running \`$COMMAND\` %d times.\n" $REPS
+
+# Run and time the command given to the script
+for i in `seq 1 $REPS`; do
+    ELAPSED=$( {/usr/bin/time --format="%e" $@ > /dev/null} 2>&1 )
+
+    TOTALTIME=$(($ELAPSED+$TOTALTIME))
+done
+
+# Find average and report
+printf "Total run time: %.2f seconds\n" $TOTALTIME
+AVGTIME=$(($TOTALTIME/$REPS))
+printf "Averate time: %.2f seconds\n" $AVGTIME
